ccd65ce3ec82fcce737a173138892915a916f702
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-07-31 14:24:26 +0800

    Added expense entity for roomdb later

---
+++ app/src/main/java/com/budgething/data/local/entities/Expense.kt
@@ -0,0 +1,15 @@
+package com.budgething.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+@Entity(tableName = "expense")
+data class Expense(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val date: LocalDate,
+    val mainCategory: String,
+    val subCategory: String,
+    val name: String,
+    val amount: Double
+)
