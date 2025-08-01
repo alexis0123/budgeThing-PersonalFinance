71e60fd45f0e1bed2a6329622d8b08547e217139
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-01 11:26:16 +0800

    Added getAllExpense function to use later

--- app/src/main/java/com/budgething/data/local/expense/ExpenseDao.kt
+++ app/src/main/java/com/budgething/data/local/expense/ExpenseDao.kt
@@ -1,12 +1,17 @@
 package com.budgething.data.local.expense
 
+import androidx.annotation.RequiresPermission
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface ExpenseDao {
 
     @Insert
     suspend fun addExpense(expense: Expense)
+
+    @Query("SELECT * FROM expense")
+    suspend fun getAllExpenses(): List<Expense>
 
 }
