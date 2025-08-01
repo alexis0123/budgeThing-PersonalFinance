a051cd6bb0358b4cdd055d9ea95532a64099aca0
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-01 11:43:43 +0800

    Added Update and getExpenseBetween

--- app/src/main/java/com/budgething/data/local/expense/ExpenseDao.kt
+++ app/src/main/java/com/budgething/data/local/expense/ExpenseDao.kt
@@ -4,6 +4,8 @@ import androidx.annotation.RequiresPermission
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
+import java.time.LocalDate
 
 @Dao
 interface ExpenseDao {
@@ -11,7 +13,13 @@ interface ExpenseDao {
     @Insert
     suspend fun addExpense(expense: Expense)
 
+    @Update
+    suspend fun updateExpense(expense: Expense)
+
     @Query("SELECT * FROM expense")
     suspend fun getAllExpenses(): List<Expense>
+
+    @Query("SELECT * FROM expense WHERE date BETWEEN :start AND :end ORDER BY date ASC")
+    suspend fun getExpenseBetween(start: LocalDate, end: LocalDate)
 
 }
