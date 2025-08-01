116afb46ffb25dfc9f9a30243450a41b20b9f602
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-01 10:09:27 +0800

    Added database

---
+++ app/src/main/java/com/budgething/data/local/AppDatabase.kt
@@ -0,0 +1,62 @@
+package com.budgething.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.budgething.data.AppDatabaseCallback
+import com.budgething.data.helper.LocalDateConverter
+import com.budgething.data.helper.SubList
+import com.budgething.data.local.expense.Expense
+import com.budgething.data.local.expense.ExpenseDao
+import com.budgething.data.local.expense.category.Category
+import com.budgething.data.local.expense.category.CategoryDao
+import com.budgething.data.local.expense.item.Item
+import com.budgething.data.local.expense.item.ItemDao
+import com.budgething.data.local.income.Income
+import com.budgething.data.local.income.IncomeDao
+import com.budgething.data.local.income.IncomeType
+import com.budgething.data.local.income.type.IncomeTypeDao
+
+@Database(
+    entities = [
+        Expense::class,
+        Item::class,
+        Category::class,
+        Income::class,
+        IncomeType::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(
+    SubList::class,
+    LocalDateConverter::class
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun expenseDao(): ExpenseDao
+    abstract fun itemDao(): ItemDao
+    abstract fun categoryDao(): CategoryDao
+    abstract fun incomeDao(): IncomeDao
+    abstract fun incomeTypeDao(): IncomeTypeDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "budgething_db"
+                )
+                    .addCallback(AppDatabaseCallback(context))
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
