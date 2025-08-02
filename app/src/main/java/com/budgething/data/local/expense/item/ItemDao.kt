2bbd65e010e2eccf61c94b4385b3ba7a8b23589f
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-02 19:26:34 +0800

    Added updateItem

--- app/src/main/java/com/budgething/data/local/expense/item/ItemDao.kt
+++ app/src/main/java/com/budgething/data/local/expense/item/ItemDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.budgething.data.local.expense.item.Item
 import kotlinx.coroutines.flow.Flow
 
@@ -13,10 +14,13 @@ interface ItemDao {
     @Insert
     suspend fun addItem(item: Item)
 
+    @Update
+    suspend fun updateItem(item: Item)
+
     @Delete
     suspend fun deleteItem(item: Item)
 
     @Query("SELECT * FROM item WHERE name LIKE '%' || :q || '%'")
-    fun getItems(q: String): Flow<List<Item>>
+    fun getItemsFor(q: String): Flow<List<Item>>
 
 }
