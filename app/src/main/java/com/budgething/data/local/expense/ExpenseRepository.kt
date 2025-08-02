e4273cacbaacdf38883e24370d107e7657cf9c15
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-02 16:26:28 +0800

    Added ExpenseRepository.kt

---
+++ app/src/main/java/com/budgething/data/local/expense/ExpenseRepository.kt
@@ -0,0 +1,22 @@
+package com.budgething.data.local.expense
+
+import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
+
+class ExpenseRepository(private val dao: ExpenseDao) {
+
+    suspend fun add(expense: Expense) {
+        dao.addExpense(expense)
+    }
+
+    suspend fun update(expense: Expense) {
+        dao.updateExpense(expense)
+    }
+
+    suspend fun getAll(): List<Expense> = dao.getAllExpenses()
+
+    fun getBetween(start: LocalDate, end: LocalDate): Flow<List<Expense>> = dao.getExpensesBetween(start, end)
+
+    fun getRecent(): Flow<List<Expense>> = dao.getLastExpenses()
+
+}
