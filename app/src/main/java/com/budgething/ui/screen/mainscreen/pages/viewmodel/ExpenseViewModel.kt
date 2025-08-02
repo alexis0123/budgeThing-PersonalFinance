f9212a82fa3766aa011aec98bb51ae0c825a41c1
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-02 17:07:51 +0800

    Added Expenses this week

--- app/src/main/java/com/budgething/ui/screen/mainscreen/pages/viewmodel/ExpenseViewModel.kt
+++ app/src/main/java/com/budgething/ui/screen/mainscreen/pages/viewmodel/ExpenseViewModel.kt
@@ -7,15 +7,27 @@ import com.budgething.data.local.expense.ExpenseRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
+import java.time.DayOfWeek
+import java.time.LocalDate
 
 class ExpenseViewModel(
     private val repo: ExpenseRepository
 ): ViewModel() {
+
+    private val startOfWeek = LocalDate.now().with(DayOfWeek.MONDAY)
+    private val endOfWeek = LocalDate.now().with(DayOfWeek.SUNDAY)
 
     val recentExpenses: Flow<List<Expense>> = repo.getRecent().stateIn(
         viewModelScope,
         SharingStarted.WhileSubscribed(),
         emptyList()
     )
+
+    val expensesThisWeek: Flow<List<Expense>> = repo.getBetween(startOfWeek, endOfWeek)
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(),
+            emptyList()
+        )
 
 }
