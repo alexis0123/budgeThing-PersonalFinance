bf4b235ed1aeb0be2c2b908979de174b0276a4bf
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-04 06:30:47 +0800

    instantiated viewmodels and db here

--- app/src/main/java/com/budgething/MainActivity.kt
+++ app/src/main/java/com/budgething/MainActivity.kt
@@ -6,27 +6,41 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.budgething.data.local.AppDatabase
+import com.budgething.data.local.expense.ExpenseRepository
+import com.budgething.data.local.expense.category.CategoryRepository
+import com.budgething.data.local.expense.item.ItemRepository
 import com.budgething.ui.theme.BudgeThingTheme
 import com.budgething.ui.screen.mainscreen.MainScreen
+import com.budgething.ui.screen.mainscreen.pages.expense.NumKeyViewModel
+import com.budgething.ui.screen.mainscreen.pages.expense.dialog.ConfirmExpenseViewModel
+import com.budgething.ui.screen.mainscreen.pages.viewmodel.ExpenseViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val db = AppDatabase.getDatabase(applicationContext)
+
+        val categoryRepo = CategoryRepository(db.categoryDao())
+        val itemRepo = ItemRepository(db.itemDao())
+        val expenseRepo = ExpenseRepository(db.expenseDao())
+
+        val numKeyViewModel = NumKeyViewModel()
+        val confirmExpenseViewModel = ConfirmExpenseViewModel(categoryRepo, itemRepo)
+        val expenseViewModel = ExpenseViewModel(expenseRepo)
+
         setContent {
             BudgeThingTheme(
                 dynamicColor = false
             ) {
-                MainScreen()
+                MainScreen(
+                    numKeyViewModel,
+                    confirmExpenseViewModel,
+                    expenseViewModel
+                )
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MainPreview() {
-    BudgeThingTheme {
-        MainScreen()
     }
 }
