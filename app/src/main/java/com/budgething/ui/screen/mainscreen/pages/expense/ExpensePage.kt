f6a499382c29d977e5daf53ef0e40a604534fa0b
alexis8buban@gmail.com <alexis8buban@gmail.com>
2025-08-03 14:42:38 +0800

    Refactored

---
+++ app/src/main/java/com/budgething/ui/screen/mainscreen/pages/expense/ExpensePage.kt
@@ -0,0 +1,99 @@
+package com.budgething.ui.screen.mainscreen.pages.expense
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.budgething.ui.screen.mainscreen.pages.viewmodel.ExpenseViewModel
+import com.budgething.ui.theme.BudgeThingTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.budgething.ui.screen.mainscreen.pages.expense.top.FormattedAmountDisplay
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun ExpensePage() {
+
+    val numKeyViewModel: NumKeyViewModel = viewModel()
+    val expenseViewModel: ExpenseViewModel
+
+    val amount by numKeyViewModel.amount.collectAsState()
+    var confirmedAmount by remember { mutableStateOf("") }
+    var showDialog by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 30.dp)
+            .padding(vertical = 60.dp),
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.End
+        ) {
+            FormattedAmountDisplay(amount, 50, MaterialTheme.colorScheme.onBackground)
+        }
+
+        Column(
+            modifier = Modifier
+                .weight(1.2f),
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            HorizontalDivider(modifier = Modifier.padding(10.dp))
+            KeyPad(
+                {
+                    confirmedAmount = amount
+                    showDialog = true
+                },
+                numKeyViewModel
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ExpensePagePreview() {
+    BudgeThingTheme(
+        darkTheme = true,
+        dynamicColor = false
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.background)
+        ) {}
+        Box(modifier = Modifier.fillMaxSize()) {
+
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 115.dp),
+                shape = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp),
+                color = MaterialTheme.colorScheme.surface,
+                tonalElevation = 0.5.dp,
+                shadowElevation = 10.dp
+            ) {
+                ExpensePage()
+            }
+        }
+    }
+}
